dc4fecd40b2d074ae1dc8e0530a01a850e9fd19f
sakethkumar1 <134069726+Sakethuchiha@users.noreply.github.com>
2024-11-11 08:12:27 +0000

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,5 +1,17 @@
-def decode_message( s: str, p: str) -> bool:
+def decode_message(s: str, p: str) -> bool:
+    s_len, p_len = len(s), len(p)
+    dp = [[False] * (p_len + 1) for _ in range(s_len + 1)]
+    dp[0][0] = True
 
-# write your code here
-  
-        return False
+    for j in range(1, p_len + 1):
+        if p[j - 1] == '*':
+            dp[0][j] = dp[0][j - 1]
+
+    for i in range(1, s_len + 1):
+        for j in range(1, p_len + 1):
+            if p[j - 1] == '*':
+                dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
+            elif p[j - 1] == '?' or s[i - 1] == p[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+
+    return dp[s_len][p_len]
