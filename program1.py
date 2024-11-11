bd12b8cd49ba79e31e073716e2a1156cbf48868c
sakethkumar1 <134069726+Sakethuchiha@users.noreply.github.com>
2024-11-11 08:10:37 +0000

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,6 +1,22 @@
 class Solution:
-   
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
-                    
-        return 0
+        if not grid:
+            return 0
+
+        def dfs(x, y):
+            if x < 0 or y < 0 or x >= len(grid) or y >= len(grid[0]) or grid[x][y] != 'L':
+                return
+            grid[x][y] = 'W'
+            dfs(x + 1, y)
+            dfs(x - 1, y)
+            dfs(x, y + 1)
+            dfs(x, y - 1)
+
+        island_count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 'L':
+                    dfs(i, j)
+                    island_count += 1
+
+        return island_count
